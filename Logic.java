77270147acc01bd07c14efedd835dc519314e823
jeremu2907 <jeremu2907@gmail.com>
2022-01-18 10:30:46 -0600

    Update Logic.java

--- Logic.java
+++ Logic.java
@@ -5,27 +5,11 @@
 import java.util.ArrayList;
 // import java.util.HashMap;
 public class Logic {
-    public static boolean logic(ArrayList<Integer> tokenData){
-        // if(tokenData.size() < 3) return false;
+    public static boolean fourInARow(String[][] table){
+        for(int i = 0; i < table.length; i++){
+            String[] horizontal = {table[i]}
+        }
 
-        // HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
-        //     // <Value, idx>
-            
-        // for(int i = 0; i < tokenData.size() - 3; i++){
-        //     //Find target number from tokenData[i]
-        //     int target = 15 - i;
-
-        //     //User target to iterate hashmap
-        //     for(int j = i + 1; j < tokenData.size(); j++){
-        //         if(data.containsValue(target - tokenData.get(j)) == true)
-        //             return true;
-        //         else{
-        //             data.put(j,tokenData.get(j));
-        //         }
-        //     }
-        // }
-
-        
         return true;
     }
 }
